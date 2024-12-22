8b6403235a32cf33577de60e6081024d92a74a41
xdream oldlu <xdream@gmail.com>
2024-12-22 09:23:31 +0800

    try fix cors error.

--- app/main.py
+++ app/main.py
@@ -143,6 +143,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=[
+        "*",
         "Location",
         "X-Filename",
         "Tus-Resumable",
